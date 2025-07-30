c0b44f466a4d916438a8c5739367572d72d29812
Chennakeshava22 <busupallechennakeshava@gmail.com>
2025-07-30 13:46:25 +0000

    change

--- Infrastructure/remote_backend_s3.tf
+++ Infrastructure/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "DevOps-Project-268586"
+    bucket = "devops-project-268586"
     key    = "devops-project-1/terraform.tfstate"
     region = "us-east-1"
   }
