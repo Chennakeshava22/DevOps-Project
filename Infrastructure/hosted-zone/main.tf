6f002734df840e143a7917d3bef07c21911be004
Chennakeshava22 <busupallechennakeshava@gmail.com>
2025-07-31 05:31:22 +0000

    new

--- Infrastructure/hosted-zone/main.tf
+++ Infrastructure/hosted-zone/main.tf
@@ -1,41 +1,208 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "tag_name" {}
-variable "public_key" {}
-variable "subnet_id" {}
-variable "sg_enable_ssh_https" {}
-variable "enable_public_ip_address" {}
-variable "user_data_install_apache" {}
-variable "ec2_sg_name_for_python_api" {}
+variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
 
-output "ssh_connection_string_for_ec2" {
-  value = format("%s%s", "ssh -i /home/ubuntu/keys/aws_ec2_terraform ubuntu@", aws_instance.dev_proj_1_ec2.public_ip)
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
 }
 
-output "dev_proj_1_ec2_instance_id" {
-  value = aws_instance.dev_proj_1_ec2.id
-}
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
 
-resource "aws_instance" "dev_proj_1_ec2" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  tags = {
-    Name = var.tag_name
-  }
-  key_name                    = "aws_key"
-  subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [var.sg_enable_ssh_https, var.ec2_sg_name_for_python_api]
-  associate_public_ip_address = var.enable_public_ip_address
-
-  user_data = var.user_data_install_apache
-
-  metadata_options {
-    http_endpoint = "enabled"  # Enable the IMDSv2 endpoint
-    http_tokens   = "required" # Require the use of IMDSv2 tokens
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
   }
 }
 
-resource "aws_key_pair" "dev_proj_1_public_key" {
-  key_name   = "aws_key"
-  public_key = var.public_key
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+}variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
+
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+}variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
+
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+}variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
+
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+}variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
+
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+}variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
+
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+}variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
+
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+}variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
+
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+}variable "domain_name" {}
+variable "aws_lb_dns_name" {}
+variable "aws_lb_zone_id" {}
+
+data "aws_route53_zone" "dev_proj_1_jhooq_org" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_record" "lb_record" {
+  zone_id = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.aws_lb_dns_name
+    zone_id                = var.aws_lb_zone_id
+    evaluate_target_health = true
+  }
+}
+
+output "hosted_zone_id" {
+  value = data.aws_route53_zone.dev_proj_1_jhooq_org.zone_id
 }
